88cac5e4b63bbc3792690626449ea4e8dc5ce21f
nisha <nishavdc@gmail.com>
2019-03-10 18:08:21 +0530

    added changes to court_conter file

--- court_counter/app/src/main/java/com/example/android/court_counter/MainActivity.java
+++ court_counter/app/src/main/java/com/example/android/court_counter/MainActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-int scoreTeamA=0;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+        displayForTeamA(0);
+        displayForTeamB(0);
     }
 
     /**
@@ -23,18 +26,45 @@ int scoreTeamA=0;
     }
 
     public void addthreeforteamA(View v) {
-        scoreTeamA=scoreTeamA+3;
+        scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
     public void addtwoforteamA(View v) {
-        scoreTeamA=scoreTeamA+2;
+        scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
     public void addoneforteamA(View v) {
-        scoreTeamA=scoreTeamA+1;
+        scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addthreeforteamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addtwoforteamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addoneforteamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void reset_button(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
 
